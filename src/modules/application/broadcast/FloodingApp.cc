25b9086b362ca84268d8190547ce305efd783842
marcilio lemos <marcilio.cc.lemos@gmail.com>
2021-07-23 09:27:37 +0000

    Implementing the flooding technique

---
+++ src/modules/application/broadcast/FloodingApp.cc
@@ -0,0 +1,103 @@
+#include "FloodingApp.h"
+
+using namespace std;
+
+Define_Module(FloodingApp);
+
+void FloodingApp::initialize(int stage)
+{
+    DemoBaseApplLayer::initialize(stage);
+    if (stage == 0) {
+        // Initializing members and pointers of your application goes here
+        EV << "Initializing FloodingApp" << std::endl;
+
+        beenInAccident = false;
+        sentMessage = false;
+        lastDroveAt = simTime();
+
+
+
+    }
+    else if (stage == 1) {
+        // Initializing members that require initialized other modules goes here
+    }
+}
+
+void FloodingApp::finish()
+{
+    DemoBaseApplLayer::finish();
+    alerts.clear();
+
+}
+
+void FloodingApp::onBSM(DemoSafetyMessage* bsm)
+{
+    // Your application has received a beacon message from another car or RSU
+    // code for handling the message goes here
+}
+
+void FloodingApp::onWSM(BaseFrame1609_4* wsm)
+{
+    SafetyAlertMessage* alert = check_and_cast<SafetyAlertMessage*>(wsm);
+
+    if(!count(alerts.begin(),alerts.end(),alert->getTreeId())){
+        findHost()->getDisplayString().setTagArg("i", 1, "green");
+        if (mobility->getRoadId()[0] != ':') traciVehicle->changeRoute(alert->getDemoData(), 9999);
+        alert->setSenderAddress(myId);
+        alert->setSerial(3);
+        scheduleAt(simTime() + 2 + uniform(0.01, 0.2), alert->dup());
+        alerts.push_back(alert->getTreeId());
+    }
+}
+
+void FloodingApp::onWSA(DemoServiceAdvertisment* wsa)
+{
+    // Your application has received a service advertisement from another car or RSU
+    // code for handling the message goes here, see TraciDemo11p.cc for examples
+}
+
+void FloodingApp::handleSelfMsg(cMessage* msg)
+{
+
+    if (SafetyAlertMessage* wsm = dynamic_cast<SafetyAlertMessage*>(msg)) {
+           // send this message on the service channel until the counter is 3 or higher.
+           // this code only runs when channel switching is enabled
+           sendDown(wsm->dup());
+           wsm->setSerial(wsm->getSerial() + 1);
+           if (wsm->getSerial() >= 3) {
+               // stop service advertisements
+               stopService();
+               delete (wsm);
+           }
+           else {
+               scheduleAt(simTime() + 1, wsm);
+           }
+       }
+       else {
+           DemoBaseApplLayer::handleSelfMsg(msg);
+       }
+}
+
+void FloodingApp::handlePositionUpdate(cObject* obj)
+{
+    DemoBaseApplLayer::handlePositionUpdate(obj);
+
+    if (mobility->getSpeed() < 1) {
+            if (simTime() - lastDroveAt >= 10 && beenInAccident == false) {
+
+                if(traciVehicle->getVType()=="warning"){
+                    findHost()->getDisplayString().setTagArg("i", 1, "red");
+                    beenInAccident = true;
+                    SafetyAlertMessage* wsm = new SafetyAlertMessage();
+                    populateWSM(wsm);
+                    wsm->setDemoData(mobility->getRoadId().c_str());
+                    sendDown(wsm);
+                    alerts.push_back(wsm->getTreeId());
+                }
+            }
+        }
+    else {
+        lastDroveAt = simTime();
+    }
+
+}
---
+++ src/modules/application/broadcast/FloodingApp.h
@@ -0,0 +1,35 @@
+#ifndef STOPAPP_H
+#define STOPAPP_H
+
+#include<omnetpp.h>
+#include "veins/modules/application/ieee80211p/DemoBaseApplLayer.h"
+#include "modules/messages/SafetyAlertMessage_m.h"
+
+
+using namespace omnetpp;
+using namespace veins;
+using namespace std;
+
+class FloodingApp : public DemoBaseApplLayer  {
+
+    public:
+        void initialize(int stage) override;
+        void finish() override;
+
+    protected:
+
+        bool beenInAccident;
+        bool sentMessage;
+        simtime_t lastDroveAt;
+        vector<long> alerts;
+
+        void onBSM(DemoSafetyMessage* bsm) override;
+        void onWSM(BaseFrame1609_4* wsm) override;
+        void onWSA(DemoServiceAdvertisment* wsa) override;
+
+        void handleSelfMsg(cMessage* msg) override;
+        void handlePositionUpdate(cObject* obj) override;
+
+};
+
+#endif
